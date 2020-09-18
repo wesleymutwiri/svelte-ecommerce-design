d4d52a86d5ac5c450ab670911766bfb50be40729
wesleymutwiri <wesleymochiemo@gmail.com>
2020-09-18 22:56:28 +0300

    improved input range and newer featuers

--- src/Components/UI/Store/Sidebar.svelte
+++ src/Components/UI/Store/Sidebar.svelte
@@ -1,3 +1,7 @@
+<script>
+  import PriceSlider from "../Product/PriceSlider.svelte";
+</script>
+
 <div class="sidebar">
   <div class="container">
     <h2 class="sidebar-header">Categories</h2>
@@ -11,26 +15,7 @@
   <div class="container">
     <h2 class="sidebar-header">Filter by</h2>
     <h4>Price</h4>
-    <div class="price-slider">
-      <span>
-        <input type="number" value="5000" min="0" max="120000" />
-        <input type="number" value="50000" min="0" max="120000"
-      /></span>
-
-      <svg width="100%" height="24">
-        <line
-          x1="4"
-          y1="0"
-          x2="300"
-          y2="0"
-          stroke="#212121"
-          stroke-width="12"
-          stroke-dasharray="1 28"
-        ></line>
-      </svg>
-      <input value="25000" min="0" max="120000" step="500" type="range" />
-      <input value="50000" min="0" max="120000" step="500" type="range" />
-    </div>
+    <PriceSlider />
   </div>
 
   <div class="container">
@@ -54,8 +39,28 @@
     </div>
   </div>
   <div class="container">
-    <h5 class="sidebar-header"><a href="#">Pet-Friendly</a></h5>
-    <h5 class="sidebar-header"><a href="#">Air-purifying</a></h5>
+    <h5 class="sidebar-header">
+    <label class="checkbox-container">
+      Pet-Friendly
+      <input type="checkbox" name="pet-friendly" id="">
+      <span class="checkmark"></span>
+    </label>
+    </h5>
+    <h5 class="sidebar-header">
+      <label class="checkbox-container">
+        Air-purifying
+        <input type="checkbox" name="air-purifying" id="">
+        <span class="checkmark"></span>
+      </label>
+    </h5>
+    <!-- <label class="container">One
+  <input type="checkbox" checked="checked">
+  <span class="checkmark"></span>
+</label>
+<label class="container">Two
+  <input type="checkbox">
+  <span class="checkmark"></span>
+</label> -->
   </div>
   <div class="container">
     <h2 class="sidebar-header">Popular Tags</h2>
@@ -117,6 +122,7 @@
     border-radius: 50%;
     height: 30px;
     width: 30px;
+    cursor: pointer;
   }
   .green {
     background: #216200;
@@ -130,4 +136,70 @@
   .brown {
     background: #780a09;
   }
+  /* The container */
+.checkbox-container {
+  display: block;
+  position: relative;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+/* Hide the browser's default checkbox */
+.checkbox-container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 200px;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.checkbox-container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.checkbox-container input:checked ~ .checkmark {
+  /* background-color: #2196F3; */
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.checkbox-container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.checkbox-container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+  
 </style>
