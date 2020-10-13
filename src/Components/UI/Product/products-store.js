b3b71c0320599cfd650f1e1160d6292752999062
wesleymutwiri <wesleymochiemo@gmail.com>
2020-10-13 19:58:44 +0300

    fixed store errors

--- src/Components/UI/Product/products-store.js
+++ src/Components/UI/Product/products-store.js
@@ -6,21 +6,25 @@ export const products = writable([
     title: "A Book",
     price: 9.99,
     description: "A great book!",
+    slug: "a-book",
   },
   {
     id: "p2",
     title: "A Carpet",
     price: 99.99,
     description: "Red and green.",
+    slug: "a_carpet",
   },
   {
     id: "p3",
     title: "Test",
     price: 9.99,
+    slug: "test",
   },
   {
     id: "p4",
     title: "Test",
     price: 9.99,
+    slug: "test_4",
   },
 ]);
