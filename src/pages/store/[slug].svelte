9861f8111f58dac217f547e093c6f4c6ac8c21e9
wesleymutwiri <wesleymochiemo@gmail.com>
2020-10-13 19:48:01 +0300

    fixed slug functions

---
+++ src/pages/store/[slug].svelte
@@ -0,0 +1,14 @@
+<script content="module">
+  import { params } from "@sveltech/routify";
+  import { products } from "../../Components/UI/Product/products-store.js";
+
+  let item = $products.find((product) => product.slug === $params.slug);
+
+  console.log($params.slug);
+  console.log($products);
+  console.log(item);
+</script>
+
+<p>{item.title}</p>
+<p>{item.description}</p>
+<p>{item.price}</p>
